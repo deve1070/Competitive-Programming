d36cfad61607c4076f8daef8a377f1f1e9f086dd
Dawit Mekonnen Kasa <99678682+deve1070@users.noreply.github.com>
2026-03-16 21:56:05 +0300

    Add solution for Maximum Subarray

--- leetcode/0053-maximum-subarray/0053-maximum-subarray.py
+++ leetcode/0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,20 +1,14 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-       
-        # Initialize the current sum and max sum with the first element of the array
-        current_sum = max_sum = nums[0]
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #[-2,-1,-4,0,-1,1,2,-3,1]
+        # 2-(-4) -0 0=6
+        # 1-(-2) +-2=1
+        # -3 +2 -2=-3 
+        current_sum = nums[0]
+        best_sum = nums[0]
 
-        # Iterate through the array starting from the second element
-        for num in nums[1:]:
-            # Update current_sum to either the current number or extend the subarray with the current number
-            current_sum = max(num, current_sum + num)
-            # Update max_sum if current_sum is larger
-            max_sum = max(max_sum, current_sum)
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            best_sum = max(best_sum, current_sum)
 
-        return max_sum
-
-   
+        return best_sum
